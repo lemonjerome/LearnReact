28a042f24d99eb1cf09b793dba1a383a5b012dbe
Gab Ramos <gabrielcatadmanramos@gmail.com>
2024-01-31 12:59:27 +0800

    netninja ep7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("hello, ninjas!");
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("hello " + name);
+        console.log(e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>CLick me</button>
+            <button onClick={(e) => {handleClickAgain("mario", e)}}>CLick mme again</button>
         </div>
     );
 }
