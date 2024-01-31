3ad4885f859f77e25146b36bb2fd68e4525679b4
Gab Ramos <gabrielcatadmanramos@gmail.com>
2024-01-31 21:43:27 +0800

    netninja ep11

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,16 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-
-    const handleClick = () => {
-
-        if (name == 'mario'){
-            setName('luigi');
-        } else{
-            setName('mario');
-        }
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick}>CLick me</button>
+            <BlogList blogs = {blogs} title = "All Blogs"></BlogList>
         </div>
     );
 }
