0be427f89d321aa800e37f64117461b7ec069671
Gab Ramos <gabrielcatadmanramos@gmail.com>
2024-02-02 22:12:36 +0800

    netninja ep18

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,27 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {fetch('http://localhost:8000/blogs')
         .then((res) => {
             return res.json()
         })
         .then ((data) => {
-            setBlogs(data)
-        })
+            setBlogs(data);
+            setIsPending(false);
+        })}, 3000)
     }, []);
+
+
+
 
     return (
         <div className="home">
+            {isPending && <p>Loading...</p>}
             {blogs && <BlogList blogs = {blogs} title = "All Blogs"></BlogList>}
-            {blogs && <BlogList blogs = {blogs.filter((blog) => (blog.author == 'mario'))} title="Mario's Blogs"></BlogList>}
         </div>
     );
 }
