fc8ca32c69678e5a07379233da3445e66b4fb711
Gab Ramos <gabrielcatadmanramos@gmail.com>
2024-02-02 22:34:54 +0800

    netninja ep20

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,9 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        setTimeout(() => {fetch('http://localhost:8000/blogss')
-        .then((res) => {
-            if (!res.ok){
-                throw Error('could not fetch');
-            }
-            return res.json();
-        })
-        .then ((data) => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null)
-        })
-        .catch(err => {
-            setError(err.message);
-            setIsPending(false);
-        })
-    }, 3000)
-    }, []);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
 
 
