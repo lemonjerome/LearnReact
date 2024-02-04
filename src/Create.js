73ae08d665e8684072f2e2714bd89570a9f2b3fb
Gab Ramos <gabrielcatadmanramos@gmail.com>
2024-02-04 20:42:14 +0800

    netninja ep30

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
     const [isPending, setIsPending] = useState(false)
+    const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,6 +22,7 @@ const Create = () => {
         })
         .then(() => {
             setIsPending(false);
+            navigate('/');
         })
     }
 
